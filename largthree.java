33005ad582362de367c7f654c3ee0b91be9bb480
DEEPBHUVA <93590755+DEEPBHUVA@users.noreply.github.com>
2022-03-29 20:50:49 +0530

    Add files via upload

---
+++ largthree.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class largthree {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first number:");
+	double a=sc.nextDouble();
+	System.out.println("Enter second number:");
+	double b=sc.nextDouble();
+	System.out.println("Enter third number:");
+	double c=sc.nextDouble();
+	double max = (a>b)?((a>c)?a:c) : ((b>c)?b:c);
+	System.out.println("This number is largest number:" +max);
+}
+}
