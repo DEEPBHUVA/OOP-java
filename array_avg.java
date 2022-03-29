6e73ad32f883d1c5b25b46ce9e525dc6c0314250
DEEPBHUVA <93590755+DEEPBHUVA@users.noreply.github.com>
2022-03-29 22:14:13 +0530

    Add files via upload

---
+++ array_avg.java
@@ -0,0 +1,14 @@
+package array_and_string;
+
+public class array_avg {
+	public static void main(String[] args) {
+		int value[] = {45,56,89,78};
+		int sum=0;
+		for(int elements:value)
+		{
+			sum=sum+elements;
+		}
+		System.out.println("The value of the sum:" +sum);
+		System.out.println("The Average of value:" +sum/4);
+	}
+}
