f4420682a028659b5e1c69278625f727ac47994b
DEEPBHUVA <93590755+DEEPBHUVA@users.noreply.github.com>
2022-04-03 00:20:11 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+package datatypes_and_operators;
+import java.util.Scanner;
+public class sum {
+public static void main(String[] args) {
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter 1st Number:");
+			double a = s.nextDouble();
+			System.out.println("Enter 2nd Number:");
+			double b = s.nextDouble();
+			double c=a+b;
+			System.out.println(c);
+}
+}
