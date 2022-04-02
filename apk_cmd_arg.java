737c34f39188fcb7632d8d3d4007f9b02a317352
DEEPBHUVA <93590755+DEEPBHUVA@users.noreply.github.com>
2022-04-03 00:21:01 +0530

    Add files via upload

---
+++ apk_cmd_arg.java
@@ -0,0 +1,11 @@
+package array_and_string;
+import java.util.Scanner;
+public class apk_cmd_arg {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String srt=sc.nextLine();
+
+		if(srt.charAt(0)>='a' && srt.charAt(0)<='z')
+		System.out.println("terminate");
+}
+}
